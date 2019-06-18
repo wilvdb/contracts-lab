52ed1564372e2ef3747ceb2bf64b10b298cb1ee6
Wil <wilfried.vandenberghe@gmail.com>
2019-06-18 10:31:58 +0200

    Fix test for  generated application

--- petclinic-gen/src/test/java/com/bil/petclinic/PetclinicApplicationTests.java
+++ petclinic-gen/src/test/java/com/bil/petclinic/PetclinicApplicationTests.java
@@ -1,12 +1,13 @@
 package com.bil.petclinic;
 
+import com.bil.petclinicgen.PetclinicgenApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PetclinicgenApplication.class)
 public class PetclinicApplicationTests {
 
     @Test
