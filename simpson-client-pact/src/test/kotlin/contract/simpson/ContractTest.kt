96f6801a950c74ddfca371b145243087113273e9
Wil <wilfried.vandenberghe@gmail.com>
2020-06-09 11:41:30 +0200

    Produce an additional jar containing only pact files Add matcher on Donuts object

--- simpson-client-pact/src/test/kotlin/contract/simpson/ContractTest.kt
+++ simpson-client-pact/src/test/kotlin/contract/simpson/ContractTest.kt
@@ -1,9 +1,12 @@
 package contract.simpson
 
 import au.com.dius.pact.consumer.MockServer
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt
 import au.com.dius.pact.consumer.junit5.PactTestFor
+import au.com.dius.pact.core.model.RequestResponsePact
 import au.com.dius.pact.core.model.annotations.Pact
 import com.bil.contract.simpson.Donut
 import com.bil.contract.simpson.DonutsClient
@@ -17,32 +20,46 @@ import org.junit.jupiter.api.extension.ExtendWith
 class ContractTest {
 
     @Pact(provider = "donuts-factory-pact", consumer = "simpson-pact")
-    fun createDonut(builder: PactDslWithProvider) = builder
-            .given("creating successfully a donut")
-            .uponReceiving("all ingredients for a donut")
-                .path("/donuts")
-                .method("POST")
-                .headers("Content-Type", "application/json")
-                .body("{\"butter\": 120, \"sugar\": 150, \"flour\": 200}")
-            .willRespondWith()
-                .status(201)
-            .given("failing to create a donut")
-            .uponReceiving("some missing ingredients")
-                .path("/donuts")
-                .method("POST")
-                .headers("Content-Type", "application/json")
-                .body("{\"butter\":-100, \"sugar\": 150, \"flour\": 200}")
-            .willRespondWith()
-                .status(400)
-            .given("getting all donuts")
-            .uponReceiving("every produced donuts by the factory")
-                .path("/donuts")
-                .method("GET")
-            .willRespondWith()
-                .status(200)
-                .body("[{\"butter\": 120, \"sugar\": 150, \"flour\": 200}]")
-                .headers(responseHeaders())
-            .toPact()
+    fun createDonut(builder: PactDslWithProvider): RequestResponsePact {
+        val donutsBody = PactDslJsonArray().minArrayLike(1)
+                .integerType("butter")
+                .integerType("sugar")
+                .integerType("flour")
+                .closeObject()
+
+        val donutBody = PactDslJsonBody()
+                .integerType("butter")
+                .integerType("sugar")
+                .integerType("flour")
+
+        return builder
+                .given("creating successfully a donut")
+                .uponReceiving("all ingredients for a donut")
+                    .path("/donuts")
+                    .method("POST")
+                    .headers("Content-Type", "application/json")
+                    .body(donutBody)
+                .willRespondWith()
+                    .status(201)
+                .given("failing to create a donut")
+                .uponReceiving("some missing ingredients")
+                    .path("/donuts")
+                    .method("POST")
+                    .headers("Content-Type", "application/json")
+                    .body("{\"butter\":-100, \"sugar\": 150, \"flour\": 200}")
+                .willRespondWith()
+                    .status(400)
+                .given("getting all donuts")
+                .uponReceiving("every produced donuts by the factory")
+                    .path("/donuts")
+                    .method("GET")
+                .willRespondWith()
+                    .status(200)
+                    .body(donutsBody)
+                    .headers(responseHeaders())
+                .toPact()
+    }
+
 
     private fun responseHeaders() = mapOf("Content-Type" to "application/json")
 
